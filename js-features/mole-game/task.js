b1ca308472770f7f76abec423198a86dcc65b226
Dilara Gilfanova <dgilfanova12@gmail.com>
2025-09-05 14:55:04 +0300

    second commit

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -14,7 +14,7 @@ for (let i = 1; i <= 9; i++) {
   const hole = getHole(i);
 
   hole.onclick = function () {
-    if (hole.classList.contains('hole hole_has-mole')) {      
+    if (hole.classList.contains('hole_has-mole')) {      
       deadCounter.textContent = parseInt(deadCounter.textContent) + 1;
     } else {     
       lostCounter.textContent = parseInt(lostCounter.textContent) + 1;
