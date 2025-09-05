4608acac01d86543cd22bf18a99a7e7e81fc246f
Dilara Gilfanova <dgilfanova12@gmail.com>
2025-09-05 14:53:07 +0300

    second commit

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,16 +1,24 @@
-const clickerTab = document.getElementById('cookie');
+const cookie = document.getElementById("cookie");
+const clickerCounter = document.getElementById("clicker__counter");
 
-clickerTab.onclick = function(){
-    const counter = document.getElementById('clicker__counter');
-    let counterValue = parseInt(counter.textContent);
-    counterValue += 1;
-    counter.textContent = counterValue;
+cookie.addEventListener("click", function () {
+  cookie.width = ++clickerCounter.textContent % 2 ? 250 : 200;
+});
 
-    const image = document.getElementById('cookie');
-    if(image.width === 200){
-        image.width = 190;
-    } else {
-        image.width = 200;
-    }
-    
-}
+// const clickerTab = document.getElementById('cookie');
+
+// clickerTab.onclick = function(){
+//     const counter = document.getElementById('clicker__counter');
+//     let counterValue = parseInt(counter.textContent);
+//     counterValue += 1;
+//     counter.textContent = counterValue;
+
+//     const image = document.getElementById('cookie');
+//     if(image.width === 200){
+//         image.width = 190;
+//     } else {
+//         image.width = 200;
+//     }    
+// }
+
+
