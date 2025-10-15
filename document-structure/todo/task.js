37a2ffbffb3b06e33fe05b4e3c245e84394240ec
Dilara Gilfanova <dgilfanova12@gmail.com>
2025-10-15 16:44:16 +0300

    second commit

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,38 +2,46 @@ const tasksList = document.getElementById('tasks__list');
 const input = document.getElementById('task__input');
 const btn = document.getElementById('tasks__add');
 
+window.addEventListener('load', loadTasks);
+
 btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    const text = input.value;
-    if(!text) return;
+	event.preventDefault();
+	const title = input.value;
+	if (!title) return;
 
-    const task = document.createElement('div');
-    task.classList.add('task');
-
-    const title = document.createElement('div');
-    title.classList.add('task__title');
-    title.textContent = text;
-
-    const remove = document.createElement('a');
-    remove.classList.add('task__remove');
-    remove.href = "#";    
-    remove.addEventListener('click', (e) => {
-        e.preventDefault();
-        tasksList.removeChild(task);
-        saveTasks();
-    })
-
-    task.appendChild(title);
-    task.appendChild(remove);
-    tasksList.appendChild(task);  
-
-    saveTasks();
+	addTask(title);
+	saveTasks();
+	input.value = '';
 })
 
+function addTask(title) {
+	tasksList.insertAdjacentHTML('afterbegin', `
+    <div class="task">
+      <div class="task__title">
+      ${title}
+      </div>
+      <a href="#" class="task__remove">&times;</a>
+    </div>
+    `);
+
+	const removeBtn = document.querySelector('.task__remove');
+	removeBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		e.target.closest('.task').remove();
+		saveTasks();
+	})
+}
+
 function saveTasks() {
-    const tasks = [];
-    document.querySelectorAll('.task__title').forEach(element => {
-        tasks.push(element.textContent);
-    });
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+	const tasks = [];
+	document.querySelectorAll('.task__title').forEach(element => {
+		tasks.push(element.textContent);
+	});
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+	const saved = JSON.parse(localStorage.getItem('tasks')) || [];
+
+	saved.forEach(title => addTask(title));
 }
