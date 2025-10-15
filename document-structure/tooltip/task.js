9e89dc92a9cee7cc8781db7d82dcbe73c69854b5
Dilara Gilfanova <dgilfanova12@gmail.com>
2025-10-15 14:55:06 +0300

    second commit

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,19 +5,25 @@ document.body.appendChild(div);
 
 const links = Array.from(document.querySelectorAll('.has-tooltip'));
 
+let activeLink = null;
+
 links.forEach((link) => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
 
-        const rect = link.getBoundingClientRect();
+        if(activeLink === link){
+            div.classList.toggle('tooltip_active'); 
+            return;
+        } else {
+            activeLink = link;
+            div.textContent = link.getAttribute('title');
+            div.classList.add('tooltip_active'); 
 
-        div.style.position = 'absolute';
-        div.style.left = rect.left + window.scrollX + 'px';
-        div.style.top = rect.bottom + window.scrollY + 5 + 'px';
-
-        div.textContent = link.getAttribute('title');
-        
-        div.classList.add('tooltip_active');       
+            const rect = link.getBoundingClientRect();
+            div.style.position = 'absolute';
+            div.style.left = rect.left + window.scrollX + 'px';
+            div.style.top = rect.bottom + window.scrollY + 5 + 'px';
+        }                   
     })
 })
 
