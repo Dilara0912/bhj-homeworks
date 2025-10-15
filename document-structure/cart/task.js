ee58c3d02136a612c5ffadc0eec012d40bae3ed5
Dilara Gilfanova <dgilfanova12@gmail.com>
2025-10-15 17:34:04 +0300

    second commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,52 +1,70 @@
 const products = document.querySelectorAll('.product');
 
+window.addEventListener('load', loadTasks);
+
 products.forEach(product => {
-    const decBtn = product.querySelector('.product__quantity-control_dec');
-    const incBtn = product.querySelector('.product__quantity-control_inc');
-    const value = product.querySelector('.product__quantity-value');
-    
-    incBtn.addEventListener('click', () => {
-    let countValue = parseInt(value.textContent);    
-    value.textContent = countValue + 1;    
-    });
+	const decBtn = product.querySelector('.product__quantity-control_dec');
+	const incBtn = product.querySelector('.product__quantity-control_inc');
+	const value = product.querySelector('.product__quantity-value');
 
-    decBtn.addEventListener('click', () => {
-        let countValue = parseInt(value.textContent); 
-        if(countValue > 1){
-        value.textContent = countValue - 1; 
-        }
-    });
+	incBtn.addEventListener('click', () => {
+		let countValue = parseInt(value.textContent);
+		value.textContent = countValue + 1;
+	});
 
-    const productAdd = product.querySelector('.product__add');
+	decBtn.addEventListener('click', () => {
+		let countValue = parseInt(value.textContent);
+		if (countValue > 1) {
+			value.textContent = countValue - 1;
+		}
+	});
 
-    productAdd.addEventListener('click', () => {
-        const productId = product.dataset.id;
-        const bin = document.querySelector('.cart__products');
-        
-        let productInBin = bin.querySelector(`.cart__product[data-id="${productId}"]`);
+	const productAdd = product.querySelector('.product__add');
 
-        if(productInBin){
-            const countDiv = productInBin.querySelector('.cart__product-count');
-            countDiv.textContent = Number(countDiv.textContent) + Number(value.textContent);
-        } else {
-            const div = document.createElement('div');
-            div.classList.add('cart__product');        
-            div.dataset.id = productId;
+	productAdd.addEventListener('click', () => {
+		const productId = product.dataset.id;
+		const bin = document.querySelector('.cart__products');
 
-            const img = document.createElement('img');
-            img.classList.add('cart__product-image');        
-            let productImg = product.querySelector('.product__image');
-            img.src = productImg.src;
+		let productInBin = bin.querySelector(`.cart__product[data-id="${productId}"]`);
 
-            const divCount = document.createElement('div');
-            divCount.classList.add('cart__product-count');
-            divCount.textContent = value.textContent;
-        
-            div.appendChild(img);
-            div.appendChild(divCount);
-            bin.appendChild(div);
-        }
-    })
+		if (productInBin) {
+			const countDiv = productInBin.querySelector('.cart__product-count');
+			countDiv.textContent = Number(countDiv.textContent) + Number(value.textContent);
+		} else {
+			const productImg = product.querySelector('.product__image').src;
+			addTask(productId, productImg, value.textContent);
+		}
+
+		saveTasks();
+	})
 })
 
+function addTask(id, img, count) {
+	document.querySelector('.cart__products').insertAdjacentHTML('afterbegin', `
+        <div class="cart__product" data-id="${id}">
+                <img class="cart__product-image" src="${img}">
+                <div class="cart__product-count">${count}</div>
+              </div>
+              `);
+}
 
+function saveTasks() {
+	const tasks = [];
+	document.querySelectorAll('.cart__product').forEach(element => {
+		const id = element.dataset.id;
+		const img = element.querySelector('.cart__product-image').src;
+		const count = element.querySelector('.cart__product-count').textContent;
+		tasks.push({
+			id,
+			img,
+			count
+		});
+	});
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+	const saved = JSON.parse(localStorage.getItem('tasks')) || [];
+
+	saved.forEach(task => addTask(task.id, task.img, task.count));
+}
